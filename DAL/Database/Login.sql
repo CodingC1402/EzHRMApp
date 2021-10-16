e25a3caac18d83ccfa7b0bcbde5e17d6a5bf3533
Nguyen Pham <19521918@gm.uit.edu.vn>
2021-10-16 20:15:55 +0700

    backup

--- DAL/Database/Login.sql
+++ DAL/Database/Login.sql
@@ -1,15 +1,19 @@
 DELIMITER  $$
 
 DROP PROCEDURE IF EXISTS LOGIN;
-CREATE PROCEDURE LOGIN(IN Username VARCHAR(255), IN LoginPassword VARCHAR(255), OUT AccessToken VARCHAR(255), OUT PrivilegeMask INT, OUT StaffID VARCHAR(10), OUT IsLoggedIn TINYINT)
+CREATE PROCEDURE LOGIN(IN Username VARCHAR(255), IN LoginPassword VARCHAR(255), OUT AccessToken VARCHAR(255), OUT PrivilegeMask INT, OUT StaffID VARCHAR(10), OUT IsLoggedIn TINYINT, OUT Success TINYINT)
 BEGIN
     DECLARE AccountCount TINYINT;
     DECLARE IsInUse TINYINT;
     DECLARE Bitmask INT;
-    DECLARE NewToken VARCHAR(255);
+    DECLARE NewToken VARCHAR(32);
 
+    /** 1 = Succeeded 0 = failed */
+    SET Success := 0;
     SELECT COUNT(*), DangLogIn, TK.QuyenHan INTO AccountCount, IsInUse, Bitmask FROM TAIKHOAN TK WHERE Username = TK.TaiKhoan AND LoginPassword = TK.Password;
+
     IF (AccountCount > 0) THEN
+        SET Success := 1;
         SET StaffID := (SELECT ID FROM NHANVIEN WHERE TaiKhoan = Username LIMIT 1);
         SET PrivilegeMask := Bitmask;
 
@@ -18,9 +22,9 @@ BEGIN
             SET TK.DangLogin = 1
             WHERE TK.TaiKhoan = Username AND LoginPassword = TK.Password;
 
-            SET NewToken := RAND();
+            SET NewToken := SUBSTRING(MD5(RAND()) ,1, 32);
             WHILE (EXISTS (SELECT * FROM ACCESSTOKENS WHERE Token = NewToken)) DO
-                SET NewToken := RAND();
+                SET NewToken := SUBSTRING(MD5(RAND()) ,1, 32);
             END WHILE;
 
             INSERT INTO ACCESSTOKENS(Token, Bitmask, NhanVienID, Account)
@@ -37,18 +41,27 @@ END;
 $$
 
 DROP PROCEDURE IF EXISTS  LOGOUT;
-CREATE PROCEDURE LOGOUT(IN Username VARCHAR(255), IN LoginPassword VARCHAR(255))
+CREATE PROCEDURE LOGOUT(IN AccessToken VARCHAR(32))
 BEGIN
-    DECLARE AccountCount TINYINT;
-
-    SELECT COUNT(*) INTO AccountCount FROM TAIKHOAN TK WHERE Username = TK.TaiKhoan AND LoginPassword = TK.Password;
-    IF (AccountCount > 0) THEN
+    IF (EXISTS (SELECT * FROM AccessTokens WHERE Token = AccessToken)) THEN
         UPDATE TAIKHOAN TK
         SET TK.DangLogin = 0
-        WHERE TK.TaiKhoan = Username AND LoginPassword = TK.Password;
+        WHERE TK.TaiKhoan = (SELECT T.Account FROM AccessTokens T WHERE Token = AccessToken LIMIT 1);
 
-        DELETE FROM ACCESSTOKENS WHERE Account = Username;
+        DELETE FROM ACCESSTOKENS WHERE AccessToken = Token;
     END IF;
+END;
+$$
+
+DROP PROCEDURE IF EXISTS  LOGOUTALL;
+CREATE PROCEDURE LOGOUTALL()
+BEGIN
+    DECLARE CurrentToken VARCHAR(32);
+
+    WHILE( EXISTS(SELECT * FROM ACCESSTOKENS) ) DO
+        SELECT Token INTO CurrentToken FROM ACCESSTOKENS LIMIT 1;
+        CALL LOGOUT(CurrentToken);
+    END WHILE;
 END;
 $$
 
--- DAL/Database/Testing.sql
+++ DAL/Database/Testing.sql
@@ -1,9 +1,9 @@
-INSERT INTO TAIKHOAN (TaiKhoan, Password, QuyenHan) VALUES ('CornyCornyCorn0', 'Password123', 123);
-INSERT INTO TAIKHOAN (TaiKhoan, Password, QuyenHan) VALUES ('CornyCornyCorn1', 'Password124', 126);
-INSERT INTO TAIKHOAN (TaiKhoan, Password, QuyenHan) VALUES ('CornyCornyCorn2', 'Password125', 127);
+INSERT INTO TAIKHOAN (TaiKhoan, Password, QuyenHan) VALUES ('CornyCornyCorn0', SHA2('Password123', 256), 123);
+INSERT INTO TAIKHOAN (TaiKhoan, Password, QuyenHan) VALUES ('CornyCornyCorn1', SHA2('Password124', 256), 126);
+INSERT INTO TAIKHOAN (TaiKhoan, Password, QuyenHan) VALUES ('CornyCornyCorn2', SHA2('Password125', 256), 127);
 
 DELIMITER  $$
-CALL LOGIN('CornyCornyCorn1', 'Password124', @AccessToken, @Mask, @ID, @IsLogedIn);
+CALL LOGIN('CornyCornyCorn0', 'Password123', @AccessToken, @Mask, @ID, @IsLogedIn, @Success);
 SELECT @AccessToken;
 SELECT @Mask;
 SELECT @ID;
@@ -11,7 +11,16 @@ SELECT @IsLogedIn;
 $$
 DELIMITER ;
 
-CALL LOGOUT('CornyCornyCorn0', 'Password123');
+CALL LOGOUT('6da463e7ad28a4cf8d36feb94e46a4fd');
+CALL LOGOUTALL();
+
+ UPDATE TAIKHOAN TK
+        SET TK.DangLogin = 0
+        WHERE TK.TaiKhoan = 'CornyCornyCorn0';
+
+ UPDATE TAIKHOAN TK
+        SET TK.DangLogin = 0
+        WHERE TK.TaiKhoan = 'CornyCornyCorn1';
 
 DELETE FROM accesstokens;
 SELECT * FROM accesstokens;
