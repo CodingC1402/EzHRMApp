6a488ac08a133af5a4f25c08d8d9d714f21fb669
Nguyen Pham <cornycodingcorn@gmail.com>
2021-10-25 12:42:52 +0700

    Add login (#6)
    * change layout and test hide tab with bitmask
    
    * add DAL database mapping
    
    * commit
    
    * add in viewmodel
    
    * add animation
    
    * add datagrid style
    
    * finish up the staff ui
    
    * fix the animation
    
    * fix small stuff

---
+++ ViewModel/ScheduleManagementViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel.Navigation;
+
+namespace ViewModel
+{
+    public class ScheduleManagementViewModel :  ViewModelBase
+    {
+        public override string ViewName => "(^ : BASED : ^)";
+    }
+}
