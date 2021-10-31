3c79ba052a5b513da608d637f97857a3ab392898
Nguyen Pham <19521918@gm.uit.edu.vn>
2021-10-31 10:02:30 +0700

    adding popupmessage style

---
+++ CornControls/CustomControl/Converters/AllCapConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CornControls.CustomControl.Converters
+{
+    public class AllCapConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(string))
+            {
+                string str = value as string;
+                if (str != null)
+                {
+                    return str.ToUpper();
+                }
+            }
+
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CornControls/CustomControl/PopUpMessage/PopUpMessage.cs
+++ CornControls/CustomControl/PopUpMessage/PopUpMessage.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Media.Animation;
 
 namespace CornControls.CustomControl
 {
@@ -67,13 +68,21 @@ namespace CornControls.CustomControl
             DefaultStyleKeyProperty.OverrideMetadata(typeof(PopUpMessage), new FrameworkPropertyMetadata(typeof(PopUpMessage)));
         }
 
-        public static readonly DependencyProperty MessageProperty = DependencyProperty.Register(nameof(Message), typeof(string), typeof(PopUpMessage), new PropertyMetadata("Message"));
+        public static readonly DependencyProperty MessageProperty = DependencyProperty.Register(nameof(Message), typeof(string), typeof(PopUpMessage), new PropertyMetadata("This is a templated message"));
         public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title), typeof(string), typeof(PopUpMessage), new PropertyMetadata("Title"));
         public static readonly DependencyProperty CanCloseProperty = DependencyProperty.Register(nameof(CanClose), typeof(bool), typeof(PopUpMessage), new PropertyMetadata(true));
         public static readonly DependencyProperty MessageResultProperty = DependencyProperty.Register(nameof(MessageResult), typeof(Result), typeof(PopUpMessage), new PropertyMetadata(Result.Ok));
         public static readonly DependencyProperty CurrentWindowProperty = DependencyProperty.Register(nameof(CurrentWindow), typeof(System.Windows.Window), typeof(PopUpMessage), new PropertyMetadata(null));
-       
+        
         public static readonly DependencyProperty IsOpenedProperty = DependencyProperty.Register(nameof(IsOpened), typeof(bool), typeof(PopUpMessage), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty InAnimTimeProperty = DependencyProperty.Register(nameof(InAnimTime), typeof(double), typeof(PopUpMessage), new PropertyMetadata(1.0));
+        public static readonly DependencyProperty OutAnimTimeProperty = DependencyProperty.Register(nameof(OutAnimTime), typeof(double), typeof(PopUpMessage), new PropertyMetadata(1.0));
+       
+        public static readonly DependencyProperty BackgroundOpacityProperty = DependencyProperty.Register(nameof(BackgroundOpacity), typeof(double), typeof(PopUpMessage), new PropertyMetadata(0.5));
+
+        public static readonly DependencyProperty PanelHeightProperty = DependencyProperty.Register(nameof(PanelHeight), typeof(double), typeof(PopUpMessage), new PropertyMetadata(300.0));
+        public static readonly DependencyProperty MidPanelWidthProperty = DependencyProperty.Register(nameof(MidPanelWidth), typeof(GridLength), typeof(PopUpMessage), new PropertyMetadata(new GridLength(600, GridUnitType.Pixel)));
 
         public string Title
         {
@@ -107,6 +116,37 @@ namespace CornControls.CustomControl
             set => SetValue(IsOpenedProperty, value);
         }
 
+        public double OutAnimTime
+        {
+            get => (double)GetValue(OutAnimTimeProperty);
+            set => SetValue(OutAnimTimeProperty, value);
+        }
+        public double InAnimTime
+        {
+            get => (double)GetValue(InAnimTimeProperty);
+            set => SetValue(InAnimTimeProperty, value);
+        }
+
+        public double BackgroundOpacity
+        {
+            get => (double)GetValue(BackgroundOpacityProperty);
+            set
+            {
+                var clampedValue = Math.Clamp(value, 0, 1);
+                SetValue(BackgroundOpacityProperty, clampedValue);
+            }
+        }
+
+        public double PanelHeight
+        {
+            get => (double)GetValue(PanelHeightProperty);
+            set => SetValue(PanelHeightProperty, value);
+        }
+        public GridLength MidPanelWidth
+        {
+            get => (GridLength)GetValue(MidPanelWidthProperty);
+            set => SetValue(MidPanelWidthProperty, value);
+        }
         public static readonly RoutedEvent MessageClosedEvent = EventManager.RegisterRoutedEvent(nameof(MessageClosed), RoutingStrategy.Tunnel, typeof(RoutedEventHandler), typeof(PopUpMessage));
 
         public event RoutedEventHandler MessageClosed
@@ -114,6 +154,9 @@ namespace CornControls.CustomControl
             add { AddHandler(MessageClosedEvent, value); }
             remove { RemoveHandler(MessageClosedEvent, value); }
         }
+
+        private Border _border;
+        private Grid _innerPanel;
 
         protected virtual void RaiseMessageClosedEvent()
         {
@@ -124,5 +167,23 @@ namespace CornControls.CustomControl
 
         protected virtual void OnMessageClosed(PopUpMessage instances)
         {}
+
+        protected override void OnTemplateChanged(ControlTemplate oldTemplate, ControlTemplate newTemplate)
+        {
+            base.OnTemplateChanged(oldTemplate, newTemplate);
+            _border = newTemplate.FindName("PART_background", this) as Border;
+            _innerPanel = newTemplate.FindName("PART_innerPanel", this) as Grid;
+        }
+
+        protected virtual AnimationTimeline CreateAnimation(double from, double to, double time, double amplitude, EasingMode mode, EventHandler whenDone = null)
+        {
+            IEasingFunction ease = new BackEase { Amplitude = amplitude, EasingMode = mode };
+            var duration = new Duration(TimeSpan.FromSeconds(time / 2));
+            var anim = new DoubleAnimation(from, to, duration) { EasingFunction = ease };
+            if (whenDone != null)
+                anim.Completed += whenDone;
+            anim.Freeze();
+            return anim;
+        }
     }
 }
