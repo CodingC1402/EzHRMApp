f0aef61b416ba4308d4b3e2eb32b095eb64dc58b
Long Phi <gameofnoobs17@gmail.com>
2021-11-15 01:39:21 +0700

    update events

--- DAL/Database/DayCheckEvents.sql
+++ DAL/Database/DayCheckEvents.sql
@@ -2,9 +2,9 @@ DELIMITER $
 
 DROP EVENT IF EXISTS START_BUSINESS_HOURS_EVENT;
 CREATE EVENT START_BUSINESS_HOURS_EVENT
-ON SCHEDULE AT (
-    CURRENT_DATE() + INTERVAL 1 DAY + INTERVAL 6 HOUR /*+ INTERVAL 12 HOUR + INTERVAL 18 MINUTE*/
-    )
+ON SCHEDULE
+    EVERY 1 DAY
+    STARTS CURRENT_DATE() + INTERVAL 1 DAY + INTERVAL 6 HOUR
 ON COMPLETION PRESERVE
 DO
     BEGIN
@@ -12,31 +12,37 @@ DO
         DECLARE gioTanLamHomQua datetime;
         DECLARE gioVaoLamHomQua datetime;
 
+        DELETE FROM eventlog;
+
         SET homQua = CURRENT_DATE() - INTERVAL 1 DAY;
 
         SET gioVaoLamHomQua =
-            (SELECT ADDTIME(homQua,
-                (CASE DAYOFWEEK(homQua)
+            (SELECT ADDTIME(CURRENT_DATE() - INTERVAL 1 DAY,
+                (CASE DAYOFWEEK(CURRENT_DATE() - INTERVAL 1 DAY)
                     WHEN 1 THEN GioVaoLamChuNhat
                     WHEN 7 THEN GioVaoLamThuBay
                     ELSE GioVaoLamCacNgayTrongTuan
                 END))
             FROM thoigianbieutuan
-            WHERE DATE(ThoiDiemTao) <= homQua
+            WHERE DATE(ThoiDiemTao) <= CURRENT_DATE() - INTERVAL 1 DAY
             ORDER BY ThoiDiemTao DESC
             LIMIT 1);
 
         SET gioTanLamHomQua =
-            (SELECT ADDTIME(homQua,
-                (CASE DAYOFWEEK(homQua)
+            (SELECT ADDTIME(CURRENT_DATE() - INTERVAL 1 DAY,
+                (CASE DAYOFWEEK(CURRENT_DATE() - INTERVAL 1 DAY)
                     WHEN 1 THEN GioTanLamChuNhat
                     WHEN 7 THEN GioTanLamThuBay
                     ELSE GioTanLamCacNgayTrongTuan
                 END))
             FROM thoigianbieutuan
-            WHERE DATE(ThoiDiemTao) <= homQua
+            WHERE DATE(ThoiDiemTao) <= CURRENT_DATE() - INTERVAL 1 DAY
             ORDER BY ThoiDiemTao DESC
             LIMIT 1);
+
+        /* !!!!LOG!!!! */
+        INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+        VALUES (NOW(), 'gia tri cac bien thoi gian:', homQua, GioVaoLamHomQua, GioTanLamHomQua);
 
 /* region: Process late shift employees situation */
 
@@ -58,11 +64,19 @@ DO
         CREATE TEMPORARY TABLE we5
         SELECT * FROM working_employees;
 
+        /* !!!!LOG!!!! */
+        INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+        VALUES (NOW(), 'tao bang temp working_employees hoan tat', null, null, null);
+
         INSERT INTO sogiolamtrongngay (Ngay, IDNhanVien, SoGioLamTrongGio, SoGioLamNgoaiGio)
         SELECT homQua, ID, 0, 0
         FROM nhanvien
         WHERE NgayVaoLam <= homQua
         AND NgayThoiViec IS NULL;
+
+        /* !!!!LOG!!!! */
+        INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+        VALUES (NOW(), 'insert cac dong moi vao sogiolamtrongngay hoan tat', null, null, null);
 
         UPDATE sogiolamtrongngay s
         SET SoGioLamTrongGio =
@@ -96,8 +110,12 @@ DO
                         WHERE we4.IDNhanVien = s.IDNhanVien),
                         NOW()
                         ))
-        WHERE Ngay = homQua
+        WHERE Ngay = CURRENT_DATE() - INTERVAL 1 DAY
         AND s.IDNhanVien IN (SELECT we5.IDNhanVien FROM we5);
+
+        /* !!!!LOG!!!! */
+        INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+        VALUES (NOW(), 'Update sogiolamtrongngay hoan tat', NULL, NULL, NULL);
 
         UPDATE chamcong
         SET ThoiGianTanLam = NOW()
@@ -112,6 +130,10 @@ DO
         DROP TEMPORARY TABLE we3;
         DROP TEMPORARY TABLE we4;
         DROP TEMPORARY TABLE we5;
+
+        /* !!!!LOG!!!! */
+        INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+        VALUES (NOW(), 'Update table chamcong hoan tat', NULL, NULL, NULL);
 /* endregion */
 
 
@@ -120,7 +142,7 @@ DO
         CREATE TEMPORARY TABLE nhanvien_hientai
         SELECT *
         FROM nhanvien
-        WHERE NgayVaoLam <= homQua
+        WHERE NgayVaoLam <= CURRENT_DATE() - INTERVAL 1 DAY
         AND NgayThoiViec IS NULL;
 
         CREATE TEMPORARY TABLE nv_ht2
@@ -139,7 +161,7 @@ DO
         SELECT ThoiGianChoPhepDiTre, ThoiGianChoPhepVeSom,
                ThoiGianDiTreToiDa, ThoiGianVeSomToiDa
         FROM thamso
-        WHERE DATE(ThoiDiemTao) <= homQua - INTERVAL 1 DAY
+        WHERE DATE(ThoiDiemTao) <= CURRENT_DATE() - INTERVAL 2 DAY
         ORDER BY ThoiDiemTao
         LIMIT 1;
 
@@ -149,10 +171,14 @@ DO
         CREATE TEMPORARY TABLE time3
         SELECT * FROM timeVariables;
 
+        /* !!!!LOG!!!! */
+        INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+        VALUES (NOW(), 'tao bang temp nhanvien_hientai va timeVariables hoan tat', NULL, NULL, NULL);
+
         /* Them nhan vien vang mat vao bang NghiPhep */
         INSERT INTO nghiphep (IDNhanVien, NgayBatDauNghi, SoNgayNghi, LyDoNghi, CoPhep)
         SELECT IDNhanVien,
-               homQua,
+               CURRENT_DATE() - INTERVAL 1 DAY,
                1,
                'Tu dong them nhan vien vang mat',
                0
@@ -160,34 +186,38 @@ DO
         WHERE (
             SELECT ThoiGianVaoLam
             FROM chamcong c
-            WHERE DATE(c.ThoiGianVaoLam) = homQua
+            WHERE DATE(c.ThoiGianVaoLam) = CURRENT_DATE() - INTERVAL 1 DAY
             GROUP BY c.IDNhanVien, DATE(c.ThoiGianVaoLam)
             ORDER BY c.ThoiGianVaoLam ASC
             LIMIT 1
         ) > ADDTIME(gioVaoLamHomQua, (SELECT ThoiGianDiTreToiDa FROM timeVariables))
         UNION
-        SELECT IDNhanVien,
-               homQua,
+        SELECT n.ID as IDNhanVien,
+               CURRENT_DATE() - INTERVAL 1 DAY,
                1,
                'Tu dong them nhan vien vang mat',
                0
         FROM nhanvien_hientai n
-        WHERE n.ID NOT IN (SELECT IDNhanVien FROM chamcong c2 WHERE DATE(c2.ThoiGianVaoLam) = homQua);
+        WHERE n.ID NOT IN (SELECT IDNhanVien FROM chamcong c2 WHERE DATE(c2.ThoiGianVaoLam) = CURRENT_DATE() - INTERVAL 1 DAY);
+
+        /* !!!!LOG!!!! */
+        INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+        VALUES (NOW(), 'Them nhan vien vang mat vao bang NghiPhep hoan tat', NULL, NULL, NULL);
 
         /* Tru luong cac nhan vien vang mat hoac di tre */
         INSERT INTO truluong (Ngay, IDNhanVien, TenViPham, SoTienTru, SoPhanTramTru, GhiChu)
     /* Nhan vien vang mat */
-        SELECT homQua,
+        SELECT CURRENT_DATE() - INTERVAL 1 DAY,
                IDNhanVien,
                'VangMat',
                (SELECT TruLuongTrucTiep FROM cacloaivipham WHERE TenViPham = 'VangMat'),
                (SELECT TruLuongTheoPhanTram FROM cacloaivipham WHERE TenViPham = 'VangMat'),
                'Tu dong tru luong nhan vien vang mat'
         FROM nghiphep
-        WHERE NgayBatDauNghi = homQua AND CoPhep = 0
+        WHERE NgayBatDauNghi = CURRENT_DATE() - INTERVAL 1 DAY AND CoPhep = 0
         UNION
     /* Nhan vien di tre */
-        SELECT homQua,
+        SELECT CURRENT_DATE() - INTERVAL 1 DAY,
                IDNhanVien,
                'DiTre',
                (SELECT TruLuongTrucTiep FROM cacloaivipham WHERE TenViPham = 'DiTre'),
@@ -197,12 +227,20 @@ DO
         WHERE (
             SELECT ThoiGianVaoLam
             FROM chamcong c
-            WHERE DATE(c.ThoiGianVaoLam) = homQua
+            WHERE DATE(c.ThoiGianVaoLam) = CURRENT_DATE() - INTERVAL 1 DAY
             GROUP BY c.IDNhanVien, DATE(c.ThoiGianVaoLam)
             ORDER BY c.ThoiGianVaoLam ASC
             LIMIT 1
         ) BETWEEN ADDTIME(gioVaoLamHomQua, (SELECT ThoiGianChoPhepDiTre FROM time2)) + INTERVAL 1 SECOND
             AND (SELECT ThoiGianDiTreToiDa FROM time3);
+
+        DROP TEMPORARY TABLE timeVariables;
+        DROP TEMPORARY TABLE time2;
+        DROP TEMPORARY TABLE time3;
+
+        /* !!!!LOG!!!! */
+        INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+        VALUES (NOW(), 'Tru luong nhan vien vang mat hoac di tre hoan tat', NULL, NULL, NULL);
 /* endregion */
 
 
@@ -213,6 +251,10 @@ DO
             SELECT NgayTinhLuongThangNay from cachtinhluong WHERE Ten = 'TheoThang'
         )) THEN
         BEGIN
+            /* !!!!LOG!!!! */
+            INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+            VALUES (NOW(), 'Hom nay la ngay tra luong cho nhan vien theo thang', NULL, NULL, NULL);
+
             INSERT INTO luong (NgayTinhLuong, IDNhanVien, TienLuong, TienTruLuong, TienThuong, TongTienLuong)
             SELECT DISTINCT
                    CURRENT_DATE(),
@@ -221,7 +263,7 @@ DO
                    (
                     SELECT SUM(SoTienTru + SoPhanTramTru * c.TienLuongMoiThang)
                     FROM truluong t
-                    WHERE (Ngay BETWEEN c2.LanTraLuongCuoi AND homQua)
+                    WHERE (Ngay BETWEEN c2.LanTraLuongCuoi AND CURRENT_DATE() - INTERVAL 1 DAY)
                     AND t.IDNhanVien = n.ID
                    ),
                    (
@@ -229,7 +271,7 @@ DO
                     (
                         SELECT HOUR(TIMEDIFF(GioTanLamCacNgayTrongTuan, GioVaoLamCacNgayTrongTuan))
                         FROM thoigianbieutuan
-                        WHERE DATE(ThoiDiemTao) <= homQua
+                        WHERE DATE(ThoiDiemTao) <= CURRENT_DATE() - INTERVAL 1 DAY
                     )
                    ),
                    c.TienLuongMoiThang
@@ -238,8 +280,12 @@ DO
             INNER JOIN sogiolamtrongngay s on s.IDNhanVien = n.ID
             INNER JOIN cachtinhluong c2 on c.CachTinhLuong = c2.Ten
             WHERE c.CachTinhLuong = 'TheoThang'
-            AND (s.Ngay BETWEEN c2.LanTraLuongCuoi AND homQua)
+            AND (s.Ngay BETWEEN c2.LanTraLuongCuoi AND CURRENT_DATE() - INTERVAL 1 DAY)
             GROUP BY n.ID;
+
+            /* !!!!LOG!!!! */
+            INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+            VALUES (NOW(), 'LUONG THANG: insert cac dong moi vao table luong hoan tat', NULL, NULL, NULL);
 
             UPDATE luong l
             SET /*TienTruLuong = (
@@ -270,6 +316,10 @@ DO
                 WHERE ChucVu IN (SELECT TenChucVu FROM chucvu WHERE CachTinhLuong = 'TheoThang')
             );
 
+            /* !!!!LOG!!!! */
+            INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+            VALUES (NOW(), 'LUONG THANG: Update tong tien luong trong table luong hoan tat', NULL, NULL, NULL);
+
             UPDATE cachtinhluong
             SET LanTraLuongCuoi = CURRENT_DATE()
             WHERE Ten = 'TheoThang';
@@ -281,6 +331,10 @@ DO
             (SELECT LanTraLuongCuoi + INTERVAL KyHanTraLuongTheoNgay DAY
             FROM cachtinhluong WHERE Ten = 'TheoGio')) THEN
         BEGIN
+            /* !!!!LOG!!!! */
+            INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+            VALUES (NOW(), 'Hom nay la ngay tra luong cho nhan vien theo ngay', NULL, NULL, NULL);
+
             INSERT INTO luong (NgayTinhLuong, IDNhanVien, TienLuong, TienTruLuong, TienThuong, TongTienLuong)
             SELECT DISTINCT
                    CURRENT_DATE(),
@@ -289,7 +343,7 @@ DO
                    (
                     SELECT SUM(SoTienTru)
                     FROM truluong t
-                    WHERE t.Ngay BETWEEN c3.LanTraLuongCuoi AND homQua
+                    WHERE t.Ngay BETWEEN c3.LanTraLuongCuoi AND CURRENT_DATE() - INTERVAL 1 DAY
                     AND t.IDNhanVien = n.ID
                    ),
                    c.TienLuongMoiGio * c.PhanTramLuongNgoaiGio * (SELECT SUM(s.SoGioLamNgoaiGio)),
@@ -299,15 +353,19 @@ DO
             INNER JOIN sogiolamtrongngay s ON n.ID = s.IDNhanVien
             INNER JOIN cachtinhluong c3 on c.CachTinhLuong = c3.Ten
             WHERE c.CachTinhLuong = 'TheoGio'
-            AND (s.Ngay BETWEEN c3.LanTraLuongCuoi AND homQua)
+            AND (s.Ngay BETWEEN c3.LanTraLuongCuoi AND CURRENT_DATE() - INTERVAL 1 DAY)
             GROUP BY n.ID;
+
+            /* !!!!LOG!!!! */
+            INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+            VALUES (NOW(), 'LUONG NGAY: insert cac dong moi vao table luong hoan tat', NULL, NULL, NULL);
 
             UPDATE luong l
             SET TienTruLuong = TienTruLuong - GREATEST((
                     SELECT SUM(SoPhanTramTru)
                     FROM truluong t
                     WHERE t.Ngay BETWEEN (SELECT LanTraLuongCuoi FROM cachtinhluong WHERE Ten = 'TheoGio')
-                        AND homQua
+                        AND CURRENT_DATE() - INTERVAL 1 DAY
                     AND t.IDNhanVien = l.IDNhanVien
                 ), 100) / 100 * l.TienLuong,
                 TongTienLuong = TongTienLuong + TienThuong - TienTruLuong
@@ -318,16 +376,32 @@ DO
                 WHERE ChucVu IN (SELECT TenChucVu FROM chucvu WHERE CachTinhLuong = 'TheoGio')
             );
 
+            /* !!!!LOG!!!! */
+            INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+            VALUES (NOW(), 'LUONG NGAY: Update tong tien luong trong table luong hoan tat', NULL, NULL, NULL);
+
             UPDATE cachtinhluong
             SET LanTraLuongCuoi = CURRENT_DATE()
             WHERE Ten = 'TheoGio';
         END;
         END IF;
 
+        DROP TEMPORARY TABLE nhanvien_hientai;
+        DROP TEMPORARY TABLE nv_ht2;
+        DROP TEMPORARY TABLE nv_ht3;
+        DROP TEMPORARY TABLE nv_ht4;
+        DROP TEMPORARY TABLE nv_ht5;
+
+        /* !!!!LOG!!!! */
+        INSERT INTO eventlog (LogTime, Message, HomQua, GioVaoLamHomQua, GioTanLamHomQua)
+        VALUES (NOW(), 'KET THUC EVENT.', NULL, NULL, NULL);
+
 /* endregion */
 
     END $
+DELIMITER ;
 
+DELIMITER $
 
 DROP PROCEDURE IF EXISTS UPDATE_START_BUSINESS_HOURS;
 CREATE PROCEDURE UPDATE_START_BUSINESS_HOURS()
@@ -335,12 +409,12 @@ BEGIN
     DECLARE businessHour datetime;
     SET businessHour = (
         SELECT
-        (ADDTIME(CURRENT_DATE() + INTERVAL 1 DAY,
+        ADDTIME(CURRENT_DATE() + INTERVAL 1 DAY,
             (CASE DAYOFWEEK(CURRENT_DATE() + INTERVAL 1 DAY)
                 WHEN 1 THEN GioVaoLamChuNhat
                 WHEN 7 THEN GioVaoLamThuBay
                 ELSE GioVaoLamCacNgayTrongTuan
-            END)))
+            END))
         FROM thoigianbieutuan
         WHERE ThoiDiemTao <= NOW()
         ORDER BY ThoiDiemTao DESC
@@ -348,10 +422,14 @@ BEGIN
     );
 
     ALTER EVENT START_BUSINESS_HOURS_EVENT
-    ON SCHEDULE AT businessHour
+    ON SCHEDULE
+        EVERY 1 DAY
+        STARTS businessHour
     ENABLE;
 END $
+DELIMITER ;
 
+DELIMITER $
 
 DROP EVENT IF EXISTS DAILY_UPDATE_START_BUSINESS_HOURS_EVENT;
 CREATE EVENT DAILY_UPDATE_START_BUSINESS_HOURS_EVENT
@@ -359,13 +437,13 @@ ON SCHEDULE EVERY 1 DAY
 STARTS (TIMESTAMP(CURRENT_DATE()) + INTERVAL 23 HOUR + INTERVAL 59 MINUTE /*+ INTERVAL 47 MINUTE*/)
 DO
     BEGIN
-        DECLARE ngayMai date;
-        DECLARE coLamNgayMai tinyint;
+        DECLARE homNay date;
+        DECLARE coLamHomNay tinyint;
 
-        SET ngayMai = CURRENT_DATE() + INTERVAL 1 DAY;
-        SET coLamNgayMai = (
+        SET homNay = CURRENT_DATE();
+        SET coLamHomNay = (
             SELECT (
-                CASE DAYOFWEEK(ngayMai)
+                CASE DAYOFWEEK(homNay)
                     WHEN 2 THEN CoLamThuHai
                     WHEN 3 THEN CoLamThuBa
                     WHEN 4 THEN CoLamThuTu
@@ -380,19 +458,22 @@ DO
             );
 
         /* check if business is currently having a holiday */
-        CREATE TEMPORARY TABLE incomingHolidays
+        CREATE TEMPORARY TABLE currentHolidays
         SELECT *
         FROM nghile
-        WHERE ngayMai BETWEEN MAKEDATE(YEAR(ngayMai), 1)
+        WHERE homNay BETWEEN MAKEDATE(YEAR(homNay), 1)
             + INTERVAL Thang - 1 MONTH
             + INTERVAL Ngay - 1 DAY
-        AND MAKEDATE(YEAR(ngayMai), 1)
+        AND MAKEDATE(YEAR(homNay), 1)
             + INTERVAL Thang - 1 MONTH
             + INTERVAL Ngay - 1 DAY
             + INTERVAL SoNgayNghi - 1 DAY;
 
-        IF NOT EXISTS(SELECT * FROM incomingHolidays) AND coLamNgayMai = 1 THEN
+        IF NOT EXISTS(SELECT * FROM currentHolidays) AND coLamHomNay = 1 THEN
             CALL UPDATE_START_BUSINESS_HOURS;
+        ELSE
+            ALTER EVENT START_BUSINESS_HOURS_EVENT
+            DISABLE;
         END IF;
     END $
 
--- DAL/Database/Testing.sql
+++ DAL/Database/Testing.sql
@@ -1,3 +1,34 @@
+show events;
+show create event START_BUSINESS_HOURS_EVENT;
+
+ALTER EVENT START_BUSINESS_HOURS_EVENT
+ON SCHEDULE
+EVERY 1 DAY
+STARTS NOW() + INTERVAL 15 SECOND;
+
+select * from eventlog;
+
+select * from thoigianbieutuan;
+select * from cachtinhluong;
+select * from nhanvien;
+select * from chamcong;
+select * from truluong;
+select * from nghiphep;
+select * from sogiolamtrongngay;
+
+DELETE FROM sogiolamtrongngay;
+DELETE FROM nghiphep;
+DELETE FROM chamcong;
+DELETE FROM truluong;
+
+UPDATE chamcong
+SET ThoiGianVaoLam = NOW() - INTERVAL 17 HOUR
+WHERE IDNhanVien = 'NV00000001';
+
+
+/*   test queries   */
+
+
 
 DELIMITER  $$
 CALL LOGIN('boss0', SHA2('0', 256), @AccessToken, @Mask, @ID, @IsLogedIn, @Success);
