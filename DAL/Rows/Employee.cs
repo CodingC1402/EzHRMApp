aedd38f7438e46e109e0117b747ef53734b73461
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2021-11-15 22:52:39 +0700

    what happen here

--- DAL/Rows/Employee.cs
+++ DAL/Rows/Employee.cs
@@ -123,34 +123,6 @@ namespace DAL.Rows
             var newestCheckIn = CheckInRepo.Instance.FindNewestCheckIn(ID);
             if (newestCheckIn.ThoiGianTanLam.HasValue)
                 return false;
-                var newestCheckIn = CheckInRepo.Instance.FindNewestCheckIn(ID);
-
-                if (newestCheckIn.ThoiGianTanLam.HasValue)
-                    return false;
-                var newestCheckIn = CheckInRepo.Instance.FindNewestCheckIn(ID);
-
-                if (newestCheckIn.ThoiGianTanLam.HasValue)
-                    return false;
-                var newestCheckIn = CheckInRepo.Instance.FindNewestCheckIn(ID);
-
-                if (newestCheckIn.ThoiGianTanLam.HasValue)
-                    return false;
-                var newestCheckIn = CheckInRepo.Instance.FindNewestCheckIn(ID);
-
-                if (newestCheckIn.ThoiGianTanLam.HasValue)
-                    return false;
-                var newestCheckIn = CheckInRepo.Instance.FindNewestCheckIn(ID);
-
-                if (newestCheckIn.ThoiGianTanLam.HasValue)
-                    return false;
-                var newestCheckIn = CheckInRepo.Instance.FindNewestCheckIn(ID);
-
-                if (newestCheckIn.ThoiGianTanLam.HasValue)
-                    return false;
-                var newestCheckIn = CheckInRepo.Instance.FindNewestCheckIn(ID);
-
-                if (newestCheckIn.ThoiGianTanLam.HasValue)
-                    return false;
 
             DateTime gioVaoLam = newestCheckIn.ThoiGianVaoLam.Date;
             TimeSpan time;
