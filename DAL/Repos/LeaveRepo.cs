a24b99faab253e43700608dad699350f72052912
Nguyen Pham <19521918@gm.uit.edu.vn>
2021-11-15 22:09:42 +0700

    I wanna die

--- DAL/Repos/LeaveRepo.cs
+++ DAL/Repos/LeaveRepo.cs
@@ -1,4 +1,5 @@
-﻿using DAL.Rows;
+﻿using DAL.Others;
+using DAL.Rows;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -18,5 +19,44 @@ namespace DAL.Repos
         }
 
         public static LeaveRepo Instance { get; private set; } = new LeaveRepo();
+        public static int GetLeavesDays(Employee employee, DateTime start, DateTime end)
+        {
+            List<Leave> leaves = new List<Leave>(Instance.GetAll());
+            List<Leave> employeesLeaves = new List<Leave>();
+
+            foreach(Leave leave in leaves)
+            {
+                if (leave.IDNhanVien == employee.ID)
+                {
+                    var startDate = leave.NgayBatDauNghi;
+                    var endDate = startDate.AddDays(leave.SoNgayNghi - 1);
+
+                    if (endDate >= start && endDate < end)
+                    {
+                        employeesLeaves.Add(leave);
+                    }
+                }
+            }
+            employeesLeaves.Sort(new LeaveComparer());
+
+            // Calculating the amount the days in total // Not recounting overlapped
+            int total = 0;
+            foreach (Leave leave in employeesLeaves)
+            {
+                var endLeaveDate = leave.NgayBatDauNghi.AddDays(leave.SoNgayNghi - 1);
+                endLeaveDate = endLeaveDate > end ? end : endLeaveDate;
+                start = leave.NgayBatDauNghi < start ? start : leave.NgayBatDauNghi;
+
+                var numberOfDay = (endLeaveDate - start).Days + 1;
+
+                if (numberOfDay >= 0)
+                {
+                    total += numberOfDay;
+                    start = endLeaveDate.AddDays(1);
+                }
+            }
+
+            return total;
+        }
     }
 }
---
+++ Model/EmployeeReportModel.cs
@@ -0,0 +1,33 @@
+﻿using DAL.Repos;
+using DAL.Rows;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class EmployeeReportModel
+    {
+        public int NumberOfPenalty { get; set; } = 0;
+        public int BeingLate { get; set; } = 0;
+        public int BeingAbsence { get; set; } = 0;
+        public int BeingAbsenceWithLeave { get; set; } = 0;
+
+        public static EmployeeReportModel Compile(EmployeeModel employee, DateTime start, DateTime end)
+        {
+            EmployeeReportModel report = new EmployeeReportModel();
+            var deductions = new List<Penalty>(PenaltyRepo.Instance.GetEmployeeDeductions(employee, start, end));
+            report.NumberOfPenalty = deductions.Count;
+            foreach (Penalty deduction in deductions)
+            {
+                if (deduction.TenViPham == Penalty.Absence)
+                    report.BeingAbsence++;
+                else if (deduction.TenViPham == Penalty.BeingLate)
+                    report.BeingLate++;
+            }
+
+            report.BeingAbsenceWithLeave = LeaveRepo.GetLeavesDays(employee, start, end);
+            return report;
+        }
+    }
+}
