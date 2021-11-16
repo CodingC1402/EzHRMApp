0d4c6f0675c43626e28ab5e4bdff269018c4f806
Nguyen Pham <19521918@gm.uit.edu.vn>
2021-11-16 18:52:54 +0700

    Cum on datagrid : ^)

--- ViewModel/HomeViewModel.cs
+++ ViewModel/HomeViewModel.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace ViewModel
 {
     public class HomeViewModel : Navigation.ViewModelBase
     {
         public override string ViewName => "Home";
+
+        public static HomeViewModel Instance { get; private set; }
+        public DateTime CurrentTime { get => DateTime.Now; }
+
+        private Timer _currentTimeUpdateTimer = null;
+
+        public HomeViewModel()
+        {
+            Instance?.CleanUp();
+            Instance = this;
+
+            _currentTimeUpdateTimer = new Timer();
+            _currentTimeUpdateTimer.Interval = 1000;
+            _currentTimeUpdateTimer.Elapsed += (s, e) =>
+            {
+                RaisePropertyChanged(nameof(CurrentTime));
+            };
+            _currentTimeUpdateTimer.Start();
+        }
+
+        protected override void CleanUp()
+        {
+            _currentTimeUpdateTimer.Stop();   
+        }
     }
 }
