a70728b12c33d57a79aaf574c9381e9cc3f391c1
Long Do <70971402+Longsans@users.noreply.github.com>
2021-11-17 08:25:55 +0700

    Check in and report (#43)
    * check in view and vm
    
    * edit check in view
    
    * report tabs
    
    * debug inserting new HR report every month
    
    * small edit to reports view

---
+++ DAL/Rows/MonthlyReport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Rows
+{
+    public class MonthlyReport : Row
+    {
+        public int Thang { get; set; }
+        public int Nam { get; set; }
+        public int SoNhanVienMoi { get; set; }
+        public int SoNhanVienThoiViec { get; set; }
+
+        public MonthlyReport() { }
+        
+        public MonthlyReport(MonthlyReport report)
+        {
+            Thang = report.Thang;
+            Nam = report.Nam;
+            SoNhanVienMoi = report.SoNhanVienMoi;
+            SoNhanVienThoiViec = report.SoNhanVienThoiViec;
+        }
+    }
+}
--- ViewModel/ReportsViewModel.cs
+++ ViewModel/ReportsViewModel.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ViewModel.Navigation;
 
 namespace ViewModel
 {
-    public class ReportsViewModel : Navigation.CRUDViewModelBase
+    public class ReportsViewModel : NavigationViewModel
     {
         public override string ViewName => "Reports";
+
+        public ReportsViewModel()
+        {
+            ToMonthlyReports = new NavigationCommand<MonthlyReportViewModel>(new MonthlyReportViewModel(), this, 0);
+            ViewModels.Add(ToMonthlyReports.ViewModel);
+
+            ToDailyReports = new NavigationCommand<DailyReportViewModel>(new DailyReportViewModel(), this, 0);
+            ViewModels.Add(ToDailyReports.ViewModel);
+
+            CurrentViewModel = ToDailyReports.ViewModel;
+        }
+
+        public NavigationCommand<MonthlyReportViewModel> ToMonthlyReports { get; set; }
+        public NavigationCommand<DailyReportViewModel> ToDailyReports { get; set; }
     }
 }
