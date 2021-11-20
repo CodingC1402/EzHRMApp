f4a4506d8f7bcdcab09d4c5f36757a00a4c8c611
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2021-11-20 18:38:41 +0700

    Role (#65)
    * change
    
    * changes
    
    * UI
    
    * add logic
    
    * change
    
    * forgot
    
    * add view
    
    * add
    
    * half way
    
    * ui seems stupid
    
    * update UI
    
    make it pretier but stil ugly
    
    * logic
    
    * merge
    
    * finish up logic
    
    * minor bug
    
    * new changes
    
    * forgot
    
    * finish up logic
    
    Co-authored-by: Nguyen Pham <cornycodingcorn@gmail.com>

--- ViewModel/UserInfoViewModel.cs
+++ ViewModel/UserInfoViewModel.cs
@@ -2,11 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ViewModel.Helper;
 using ViewModel.Structs;
 
 namespace ViewModel
 {
-    public class UserInfoViewModel : Navigation.ViewModelBase
+    public class UserInfoViewModel : Navigation.CRUDViewModelBase
     {
         public override string ViewName => "User Info";
 
@@ -18,11 +19,89 @@ namespace ViewModel
 
         public bool IsAvailable { get; set; }
 
+        private RelayCommand<object> _selectProfileCommand;
+        public RelayCommand<object> SelectProfileCommand => _selectProfileCommand ?? (_selectProfileCommand = new RelayCommand<object>(ExecuteSelectProfile, CanExecuteSelectProfile));
+
+        public bool CanExecuteSelectProfile(object param)
+        {
+            return true;
+        }
+
+        public void ExecuteSelectProfile(object param)
+        {
+            if (param == null)
+                return;
+
+            ProfilePicture = new Image(param as Image);
+            var employeeProfile = UserInfo.GetProfilePicture();
+
+            employeeProfile.Image = ProfilePicture.ImageBytes;
+            employeeProfile.Width = ProfilePicture.Width;
+            employeeProfile.Type = ProfilePicture.FileType;
+
+            UserInfo.ProfilePicture = employeeProfile;
+
+            var result = UserInfo.Save(false);
+
+            if (result == "")
+            {
+                base.ExecuteConfirmUpdate(param);
+                LoggedInViewModel.Instance.UpdateToEmployee();
+                StartUpdateCommand.RaiseCanExecuteChangeEvent();
+            }
+            else
+            {
+                SetCurrentModelBack();
+                ErrorString = result;
+                HaveError = true;
+            }
+        }
+
+        public override void ExecuteUpdate(object param)
+        {
+            base.ExecuteUpdate(param);
+        }
+
+        public override void ExecuteConfirmUpdate(object param)
+        {
+            var result = UserInfo.Save(false);
+
+            if (result == "")
+            {
+                base.ExecuteConfirmUpdate(param);
+                StartUpdateCommand.RaiseCanExecuteChangeEvent();
+            }
+            else
+            {
+                ErrorString = result;
+                HaveError = true;
+            }
+        }
+
+        public override void ExecuteCancleUpdate(object param)
+        {
+            base.ExecuteCancleUpdate(param);
+            SetCurrentModelBack();
+        }
+
+        public override bool CanExecuteUpdateStart(object param)
+        {
+            return base.CanExecuteUpdateStart(param) && UserInfo != null;
+        }
+
+        private void SetCurrentModelBack()
+        {
+            UserInfo = EmployeeModel.GetEmployeeByID(UserInfo.ID);
+            ProfilePicture = UserInfo != null ? new Image(UserInfo.ProfilePicture) : null;
+            SelectProfileCommand.RaiseCanExecuteChangeEvent();
+            StartUpdateCommand.RaiseCanExecuteChangeEvent();
+        }
+
         public UserInfoViewModel()
         {
             IsAvailable = true;
             Instance = this;
-            UserInfo = EmployeeModel.GetEmployeeByID(LoginInfo.EmployeeID);
+            UserInfo = LoginInfo.Employee;
             
             if (UserInfo == null)
             {
