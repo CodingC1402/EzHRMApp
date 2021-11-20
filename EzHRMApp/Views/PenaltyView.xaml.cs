67719f57d622400d4af8698e3f8e6856625152d7
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2021-11-21 02:43:09 +0700

    UI

--- EzHRMApp/Views/PenaltyView.xaml.cs
+++ EzHRMApp/Views/PenaltyView.xaml.cs
@@ -24,5 +24,49 @@ namespace EzHRMApp.Views
         {
             InitializeComponent();
         }
+
+        protected void searchChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateFilter();
+        }
+
+        protected virtual void UpdateFilter()
+        {
+            if (datagridEx.SearchText == "")
+            {
+                datagridEx.SetCollectionFilter(null);
+            }
+            else
+            {
+                //datagridEx.SetCollectionFilter(obj =>
+                //{
+                //    RoleModel role = obj as RoleModel;
+                //    if ((!showResignCheckBox.IsChecked.HasValue || !showResignCheckBox.IsChecked.Value) && role.DaXoa == 1)
+                //    {
+                //        return false;
+                //    }
+
+                //    if (datagridEx.SearchText != "")
+                //    {
+                //        var searchText = datagridEx.SearchText;
+                //        switch (datagridEx.SearchFilter)
+                //        {
+                //            case "Name":
+                //                return role.TenChucVu.Contains(searchText);
+                //            case "Salary by":
+                //                return role.CachTinhLuong.Contains(searchText);
+                //            case "Hourly Wage":
+                //                return role.TienLuongMoiGio.ToString().Contains(searchText);
+                //            case "Monthly Wage":
+                //                return role.TienLuongMoiThang.ToString().Contains(searchText);
+                //            case "Overtime %":
+                //                return role.PhanTramLuongNgoaiGio.ToString().Contains(searchText);
+                //        }
+                //    }
+
+                //    return true;
+                //});
+            }
+        }
     }
 }
--- ViewModel/PenaltyViewModel.cs
+++ ViewModel/PenaltyViewModel.cs
@@ -1,18 +1,24 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace ViewModel
 {
-    public class PenaltyViewModel : Navigation.CRUDViewModelBase
+    public class PenaltyViewModel : Navigation.ViewModelBase
     {
         public override string ViewName => "Penalty";
 
         public static PenaltyViewModel Instance { get; private set; }
+        public ObservableCollection<PenaltyModel> Penalties { get; set; }
+        public PenaltyModel SelectedPenalty { get; set; }
+
 
         public PenaltyViewModel()
         {
             Instance = this;
+            Penalties = PenaltyModel.LoadAll();
         }
     }
 }
