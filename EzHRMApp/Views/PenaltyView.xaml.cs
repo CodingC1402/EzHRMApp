e546c165b6a8a477c0bc72691ec8d1215ce3ba43
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2021-11-21 07:01:35 +0700

    Penalty (#72)
    * change
    
    * changes
    
    * UI
    
    * add logic
    
    * change
    
    * forgot
    
    * add view
    
    * add
    
    * half way
    
    * ui seems stupid
    
    * update UI
    
    make it pretier but stil ugly
    
    * logic
    
    * merge
    
    * finish up logic
    
    * minor bug
    
    * new changes
    
    * forgot
    
    * finish up logic
    
    * bug
    
    * change ui
    
    * update department view
    
    * update
    
    * commit
    
    * UI
    
    * logic
    
    Co-authored-by: Nguyen Pham <cornycodingcorn@gmail.com>

--- EzHRMApp/Views/PenaltyView.xaml.cs
+++ EzHRMApp/Views/PenaltyView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CornControls.CustomControl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,61 @@ namespace EzHRMApp.Views
         public PenaltyView()
         {
             InitializeComponent();
+        }
+
+        protected void searchChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateFilter();
+        }
+
+        protected virtual void UpdateFilter()
+        {
+            if (datagridEx.SearchText == "")
+            {
+                datagridEx.SetCollectionFilter(null);
+            }
+            else
+            {
+                //datagridEx.SetCollectionFilter(obj =>
+                //{
+                //    RoleModel role = obj as RoleModel;
+                //    if ((!showResignCheckBox.IsChecked.HasValue || !showResignCheckBox.IsChecked.Value) && role.DaXoa == 1)
+                //    {
+                //        return false;
+                //    }
+
+                //    if (datagridEx.SearchText != "")
+                //    {
+                //        var searchText = datagridEx.SearchText;
+                //        switch (datagridEx.SearchFilter)
+                //        {
+                //            case "Name":
+                //                return role.TenChucVu.Contains(searchText);
+                //            case "Salary by":
+                //                return role.CachTinhLuong.Contains(searchText);
+                //            case "Hourly Wage":
+                //                return role.TienLuongMoiGio.ToString().Contains(searchText);
+                //            case "Monthly Wage":
+                //                return role.TienLuongMoiThang.ToString().Contains(searchText);
+                //            case "Overtime %":
+                //                return role.PhanTramLuongNgoaiGio.ToString().Contains(searchText);
+                //        }
+                //    }
+
+                //    return true;
+                //});
+            }
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Popup message not work like messagebox || Need changing
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this penalty?", "Warning", MessageBoxButton.OKCancel);
+
+            if (result == MessageBoxResult.OK)
+                deleteBtn.CommandParameter = true;
+            else
+                deleteBtn.CommandParameter = false;
         }
     }
 }
--- ViewModel/PenaltyViewModel.cs
+++ ViewModel/PenaltyViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using ViewModel.Helper;
 
 namespace ViewModel
 {
@@ -9,10 +12,51 @@ namespace ViewModel
         public override string ViewName => "Penalty";
 
         public static PenaltyViewModel Instance { get; private set; }
+        public ObservableCollection<PenaltyModel> Penalties { get; set; }
+        private PenaltyModel _selectedPenalty = null;
+        public PenaltyModel SelectedPenalty
+        {
+            get => _selectedPenalty;
+            set
+            {
+                _selectedPenalty = value;
+                DeleteCommand.RaiseCanExecuteChangeEvent();
+            }
+        }
+
+        private RelayCommand<object> _deleteCommand;
+        public RelayCommand<object> DeleteCommand => _deleteCommand ?? (_deleteCommand = new RelayCommand<object>(Delete, CanDelete));
+
+        private bool CanDelete(object param)
+        {
+            return SelectedPenalty != null;
+        }
+
+        private void Delete(object param)
+        {
+            if (param == null)
+                return;
+
+            if ((bool)param)
+            {
+                string result = SelectedPenalty.Delete();
+
+                if (result == "")
+                {
+                    Penalties.Remove(SelectedPenalty);
+                }
+                else
+                {
+                    ErrorString = result;
+                    HaveError = true;
+                }
+            }
+        }
 
         public PenaltyViewModel()
         {
             Instance = this;
+            Penalties = PenaltyModel.LoadAll();
         }
     }
 }
