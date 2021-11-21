f91af0c29cf9f04e976c5f1a3a37cb8ae2975bee
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2021-11-21 12:19:44 +0700

    init

--- EzHRMApp/Views/PayrollTypesView.xaml.cs
+++ EzHRMApp/Views/PayrollTypesView.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +25,76 @@ namespace EzHRMApp.Views
         public PayrollTypesView()
         {
             InitializeComponent();
+        }
+
+        protected void searchChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateFilter();
+        }
+
+        protected virtual void UpdateFilter()
+        {
+            if (datagridEx.SearchText == "")
+            {
+                datagridEx.SetCollectionFilter(null);
+            }
+            else
+            {
+                datagridEx.SetCollectionFilter(obj =>
+                {
+                    PenaltyTypeModel role = obj as PenaltyTypeModel;
+                    if (datagridEx.SearchText != "")
+                    {
+                        var searchText = datagridEx.SearchText;
+                        switch (datagridEx.SearchFilter)
+                        {
+                            case "Name":
+                                return role.TenViPham.Contains(searchText);
+                            case "Percentage deduction":
+                                return $"{role.TruLuongTheoPhanTram.ToString("N2")}%".Contains(searchText);
+                            case "Flat deduction":
+                                return role.TruLuongTrucTiep.ToString("N2").Contains(searchText);
+                        }
+                    }
+
+                    return true;
+                });
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void PercentValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"^[0-9]*(?:\.[0-9]*)?$");
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+
+        // Clamp value
+        private void TextBoxEx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            float value = 0;
+            bool isFloat = float.TryParse(textBox.Text, out value);
+            if (!isFloat)
+            {
+                textBox.Text = "0";
+            }
+            else
+            {
+                if (value > 100)
+                {
+                    textBox.Text = "100";
+                }
+                else if (value < 0)
+                {
+                    textBox.Text = "0";
+                }
+            }
         }
     }
 }
