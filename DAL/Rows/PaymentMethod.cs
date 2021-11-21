ed7523df4d7eef8d6168014584b99757eb1ebf43
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2021-11-21 12:55:37 +0700

    rename

--- DAL/Rows/PaymentMethod.cs
+++ DAL/Rows/PaymentMethod.cs
@@ -4,20 +4,20 @@ using System.Text;
 
 namespace DAL.Rows
 {
-    public class CalculateSalary : Row
+    public class PaymentMethod : Row
     {
         public string Ten { get; set; }
         public int KyHanTraLuongTheoNgay { get; set; }
         public DateTime LanTraLuongCuoi { get; set; }
         public DateTime NgayTinhLuongThangNay { get; set; }
 
-        public CalculateSalary(CalculateSalary cs)
+        public PaymentMethod(PaymentMethod cs)
         {
             Ten = cs.Ten;
             KyHanTraLuongTheoNgay = cs.KyHanTraLuongTheoNgay;
             LanTraLuongCuoi = cs.LanTraLuongCuoi;
             NgayTinhLuongThangNay = cs.NgayTinhLuongThangNay;
         }
-        public CalculateSalary() { }
+        public PaymentMethod() { }
     }
 }
---
+++ Model/PaymentMethodModel.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Repos;
+using DAL.Rows;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Model
+{
+    public class PaymentMethodModel : PaymentMethod
+    {
+        public static ObservableCollection<PaymentMethodModel> LoadAll()
+        {
+            var list = PaymentMethodRepo.Instance.GetAll();
+            var resultList = new ObservableCollection<PaymentMethodModel>();
+
+            foreach (var item in list)
+            {
+                resultList.Add(new PaymentMethodModel(item));
+            }
+
+            return resultList;
+        }
+
+        public PaymentMethodModel(PaymentMethod cs) : base(cs) { }
+
+        public PaymentMethodModel(string ten, int kyHanTraLuongTheoNgay, DateTime lanTraLuongCuoi, DateTime ngayTinhLuongThangNay) : base()
+        {
+            Ten = ten;
+            KyHanTraLuongTheoNgay = kyHanTraLuongTheoNgay;
+            LanTraLuongCuoi = lanTraLuongCuoi;
+            NgayTinhLuongThangNay = ngayTinhLuongThangNay;
+        }
+    }
+}
